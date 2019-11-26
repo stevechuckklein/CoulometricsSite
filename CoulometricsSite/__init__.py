26d0cd666868bef121dc82868ee160c01024b60f
Stephen Klein <steve@coulometrics.com>
2019-11-26 16:35:09 -0500

    add bootstrap, restructuring

--- CoulometricsSite/__init__.py
+++ CoulometricsSite/__init__.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def homepage():
     return render_template('home.html')
 
+@app.route('/index/')
+def index():
+	return render_template('index.html')
+
 if __name__ == "__main__":
     app.run()
